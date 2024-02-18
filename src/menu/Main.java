69104dc4a785b7467b3313d847ff24512d745c0c
DvdPires19 <david.pires1997@gmail.com>
2024-02-18 17:26:16 +0100

    feat(src/menu/main): app created on Main
    1) While loop created to repeat the process of booking seats until the user decides to exit the
    program 2) Initial menu created (line 28 and 29) 3) switch case statement added with 3 different
    options(1-Get information about the halls. 2- Book seats. 3- Exit the app)

--- src/menu/Main.java
+++ src/menu/Main.java
@@ -21,5 +21,64 @@ public class Main {
         cine1.añadirSala(sala2);
         cine1.añadirSala(sala3);
         cine1.añadirSala(sala4);
+        int opcion;
+        //Bucle creado mientras seguir valga true, para poder realizar el proceso principal de la aplicación las veces que determine el usuario hasta que decida salir
+        boolean seguir = true;
+        while (seguir) {
+            System.out.println("-------------------------------------------------");
+            System.out.println("BIENVENIDO A " + cine1.getNombreCine() + ", SUS CINES DE CONFIANZA"
+                    + "\n-------------------------------------------------\n¿Qué desea hacer?\n1) Ver cartelera para hoy.\n2) Comprar entradas.\n3) Salir.");
+            opcion = entrada.nextInt();
+
+            switch (opcion) {
+                //Muestra la información de todas las salas disponibles
+                case 1:
+                    sala1.informacionSalas();
+                    sala2.informacionSalas();
+                    sala3.informacionSalas();
+                    sala4.informacionSalas();
+                    break;
+                case 2:
+                    // Reserva de entradas en función de la sala elegida
+                    int salaReservar;
+                    int fila;
+                    int columna;
+                    String email;
+                    System.out.println("Por favor, introduzca un email para gestionar su reserva: ");
+                    email = entrada.next();
+                    System.out.println("En qué sala desea reservar entrada?");
+                    salaReservar = entrada.nextInt();
+                    System.out.println("Seleccione una fila: ");
+                    fila = entrada.nextInt();
+                    System.out.println("Seleccione un asiento: ");
+                    columna = entrada.nextInt();
+                    //Llamada a los métodos de reserva y de muestra de asientos disponbles en función de la sala
+                    if (salaReservar == 1) {
+                        sala1.reservarButaca(fila, columna, email);
+                        sala1.mostrarButacas(salaReservar);
+                    }
+                    if (salaReservar == 2) {
+                        sala2.reservarButaca(fila, columna, email);
+                        sala2.mostrarButacas(salaReservar);
+                    }
+                    if (salaReservar == 3) {
+                        sala3.reservarButaca(fila, columna, email);
+                        sala3.mostrarButacas(salaReservar);
+                    }
+                    if (salaReservar == 4) {
+                        sala4.reservarButaca(fila, columna, email);
+                        sala4.mostrarButacas(salaReservar);
+                    }
+                    break;
+                case 3:
+                    System.out.println("GRACIAS POR CONFIAR EN YELMO CINES. HASTA LA PRÓXIMA!");
+                    seguir = false;
+
+
+            }
+        }
+
+
     }
-}
+    }
+
