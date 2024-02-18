9c7f2471c175043402adf948b9bee56efaf9e2b2
DvdPires19 <david.pires1997@gmail.com>
2024-02-18 16:21:37 +0100

    build(src/clases/peliculas): constructor added to Peliculas's class

--- src/clases/Peliculas.java
+++ src/clases/Peliculas.java
@@ -2,4 +2,9 @@ package clases;
 public class Peliculas {
     private String titulo;
     private int duracion;
+    public Peliculas(String titulo, int duracion) {
+        this.titulo = titulo;
+        this.duracion = duracion;
+    }
+
 }
