f7c8a8367895b3a741d15088d60e21762eda6b41
DvdPires19 <david.pires1997@gmail.com>
2024-02-18 16:32:42 +0100

    build(src/clases/salas): constructor added to Salas' class

--- src/clases/Salas.java
+++ src/clases/Salas.java
@@ -3,4 +3,10 @@ public class Salas {
     private int numero;
     private Peliculas pelicula;
     private String[][] butacas;
+
+    public Salas(int numero, Peliculas pelicula,int filas, int columnas) {
+        this.numero = numero;
+        this.pelicula = pelicula;
+        this.butacas = new String[filas][columnas];
+    }
 }
