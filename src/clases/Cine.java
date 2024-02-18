5b23824cde442562cf4909f7261944d595000b79
DvdPires19 <david.pires1997@gmail.com>
2024-02-18 16:36:29 +0100

    build(src/clases/cine): constructor added to Cine's class

--- src/clases/Cine.java
+++ src/clases/Cine.java
@@ -3,4 +3,9 @@ public class Cine {
     private String nombre;
     private int aforo;
     private Salas[] salas;
+
+    public Cine(String nombre, int numeroSalas) {
+        this.nombre = nombre;
+        this.salas = new Salas[numeroSalas];
+    }
 }
